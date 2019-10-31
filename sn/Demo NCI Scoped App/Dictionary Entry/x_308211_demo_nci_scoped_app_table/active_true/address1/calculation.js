9d274384fb31029428e9484789b49e2e5f932b8b
System Administrator <admin@example.com>
2019-10-31 13:54:34 -0400

    Changes by 'System Administrator' on 'Demo NCI Scoped App - 1.0.0' -- Build #1

---
+++ sn/Demo NCI Scoped App/Dictionary Entry/x_308211_demo_nci_scoped_app_table/active_true/address1/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : Demo NCI Scoped App
+ * ClassName   : sys_dictionary
+ * Created On  : 2019-10-31 17:53:57
+ * Created By  : admin
+ * Updated On  : 2019-10-31 17:53:57
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=91c4049edb704010343f58b3ca9619f6
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);

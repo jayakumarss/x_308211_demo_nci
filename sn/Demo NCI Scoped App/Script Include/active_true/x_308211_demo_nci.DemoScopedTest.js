fa56ff75a7d221423f15251809d192b9d861bdb5
System Administrator <admin@example.com>
2019-11-14 09:27:48 -0500

    Changes by 'System Administrator' on 'Demo NCI Scoped App - 1.0.5' -- Build #1

---
+++ sn/Demo NCI Scoped App/Script Include/active_true/x_308211_demo_nci.DemoScopedTest.js
@@ -0,0 +1,16 @@
+/* 
+ * Application : Demo NCI Scoped App
+ * ClassName   : sys_script_include
+ * Created On  : 2019-11-14 14:25:52
+ * Created By  : admin
+ * Updated On  : 2019-11-14 14:25:52
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=328b1722db410010343f58b3ca961991
+ */
+var DemoScopedTest = Class.create();
+DemoScopedTest.prototype = {
+    initialize: function() {
+    },
+
+    type: 'DemoScopedTest'
+};
